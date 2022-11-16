f6d4f8b109dacf144025ca80fa6508192225477a
Aldair Alfonso <aldairalfonsoperez@gmail.com>
2022-11-15 20:40:27 -0500

    Make a Vectorial class

--- models.py
+++ models.py
@@ -1,18 +1,13 @@
 from utils import *
 
-def vectorial_model(query:str, dataset:list, k:int = 10):
-    # query: string
-    # k: number of documents to return
-    # return: list of k documents (ids) sorted by relevance
-    
-    # data = parse_documents(dataset)
-    
-    vocabulary = get_vocabulary(dataset)
-    # print(vocabulary)
-    vectors = get_word_counts(dataset, vocabulary)
-    query_vector = get_word_counts(parse_query(query), vocabulary)
-    cosine_similarity = get_cosine_similarity(query_vector, vectors)
-    answer = sorted(cosine_similarity, key=cosine_similarity.get, reverse=True)[:k]
-    return answer
-    
-    
+class Vectorial:
+    def __init__(self, dataset: list, k: int = 10):
+        self._vocabulary = get_vocabulary(dataset)
+        self._vectors = get_word_counts(dataset, self._vocabulary)
+        self._k = k
+
+    def make_query(self, query: str):
+        query_vector = get_word_counts(parse_query(query), self._vocabulary)
+        cosine_similarity = get_cosine_similarity(query_vector, self._vectors)
+        answer = sorted(cosine_similarity, key=cosine_similarity.get, reverse=True)[:self._k]
+        return answer
